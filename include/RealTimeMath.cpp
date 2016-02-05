88b6d58fd708b51261a0a263b6f013d499b6c373
anselg <aesthapus@gmail.com>
2016-02-04 20:08:50 -0500

    format text and add copyright blurb to top of file

--- include/RealTimeMath.cpp
+++ include/RealTimeMath.cpp
@@ -1,21 +1,44 @@
+/*
+ * Copyright (C) 2011 Weill Medical College of Cornell University
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation; either version 2 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
 #include "RealTimeMath.h"
 #include <math.h>
 #include <iostream>
+
 using namespace std;
+
 RealTimeMath::RealTimeMath(){
-    powFast = new PowFast(18);
-    }
+	powFast = new PowFast(18);
+}
 
 RealTimeMath::~RealTimeMath(){
-    delete powFast;
-    }
+	delete powFast;
+}
 
 double RealTimeMath::fastEXP(double x){
-	if(x > 88.5 || x < -87){ // Overflow prevention due to powFast library's use of float
-		if( x < -746 ) // Close to 0 shortcut: If x < -746, math.h exp(x) function returns 0 due to truncation
+	if(x > 88.5 || x < -87) { // Overflow prevention due to powFast library's use of float
+
+		// Close to 0 shortcut: If x < -746, math.h exp(x) function returns 0 due to truncation
+		if( x < -746 ) 
 			return 0;
 
-		if( x > 710 ){ // Infinity shortcut: If x > 710, math.h exp(x) returns infinity
+		// Infinity shortcut: If x > 710, math.h exp(x) returns infinity
+		if( x > 710 ){ 
 			return INFINITY;
 		}
 
@@ -37,16 +60,14 @@ double RealTimeMath::fastEXP(double x){
 		}
 
 		return powAns;
-		
 	} // end overflow prevention
-		
+
 	else // call powFast e^x function
 		return powFast->e(x);
 }
 
 // POWER OPTIMIZATION
 // x^y = e^(y*ln(x))
-
 double RealTimeMath::fastPOW(double x, double y){
-    return fastEXP(y * log(x)); // Pow function using fastEXP
+	return fastEXP(y * log(x)); // Pow function using fastEXP
 }
